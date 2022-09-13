e6a73246b6e2cdfdddab27f03229ae20c4b4672a
PatrykGorka2 <90254142+PatrykGorka2@users.noreply.github.com>
2022-09-13 08:02:55 +0200

    Create index.php

---
+++ Tworzenie stron internetowych/PHP/13.09.2022/Zadanie 1 - typy danych w PHP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $liczba = 10;
+    echo('Zadanie 1 - typy danych w PHP.<br>');
+    echo($liczba);
+    echo('<div>Wartosc zmiennej o nazwie liczba wynosi: ' .$liczba .
+'</div>');
+
+echo('<div>Liczba: $liczba</div>');
+echo("<div>Liczba: $liczba</div>");
+
+echo('<div style="color:red;">tekst</div>');
+
+    ?>
+</body>
+</html>
