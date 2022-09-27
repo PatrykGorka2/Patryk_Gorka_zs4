8ff0b91315e4f9504b7ff8b3e8b26ea8d5c755bf
PatrykGorka2 <90254142+PatrykGorka2@users.noreply.github.com>
2022-09-27 07:56:58 +0200

    Create zad2.php

---
+++ Tworzenie stron internetowych/PHP/13.09.2022/Zadanie 7 - funkcje wbudowane/zad2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="t1" placeholder="TEXT">
+        <button type="submit">ZMIANA</button>
+    </form>
+    <?php
+    if(isset($_POST)){
+        $str = $_POST['t1'];
+
+        $str = strlen($str);
+
+        echo $str;
+    }
+    ?>
+    
+</body>
+</html>
