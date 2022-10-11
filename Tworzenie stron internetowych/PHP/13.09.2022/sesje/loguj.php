2da16849cd9196080b3b01048b96efa0c2805050
PatrykGorka2 <90254142+PatrykGorka2@users.noreply.github.com>
2022-10-11 08:40:56 +0200

    Create loguj.php

---
+++ Tworzenie stron internetowych/PHP/13.09.2022/sesje/loguj.php
@@ -0,0 +1,32 @@
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Zaloguj się</h1>
+    <form method="post">
+        <input type="text" name="login">
+        <input type="text" name="haslo">
+        <input type="submit" value="Zaloguj">
+    </form>
+
+    <?php
+    if(isset($_POST['login'])){
+        if(empty($_POST['login'])|| empty($_POST['haslo'])){
+            echo 'Wprowadz dane logowania';
+        }
+        else{
+            $_SESSION['login_uzytkownika'] = $_POST['login'];
+            header('Location: index.php');
+        }
+    }
+    ?>
+</body>
+</html>
