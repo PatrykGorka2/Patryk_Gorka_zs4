bb7a10d121fcc78315ed9aaa7845e167504eab64
PatrykGorka2 <90254142+PatrykGorka2@users.noreply.github.com>
2022-11-04 08:46:11 +0100

    Add files via upload

---
+++ Tworzenie stron internetowych/APLIKACJANOWALOGINHASLO./04.11.2022/serwis.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serwis</title>
+    <style>
+        body{
+            font-family: arial;
+            background-color: cornsilk;
+        }
+
+        a{
+            color: brown;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <h2>Witaj w naszym serwisie. Życze miłej pracy</h2>
+    <p>Jestes zalogowany jako: </p>
+    <a href="">Wyloguj</a>
+</body>
+</html>
